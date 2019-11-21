dbde3f99f2b28afa4121b97cc1f143ef68ee253e
Barakuda_tm <barakudatm@gmail.com>
2019-11-22 01:01:01 +0300

    Add new structure of server

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 Object.assign(global, {ROOT_DIR: '/var/www/freelancehunt/parser-lootdog'});
 
+const parser = new (require(`${ROOT_DIR}/Classes/Parser`))();
 const router = new (require(`${ROOT_DIR}/Classes/Router`))();
 const orm = new (require(`${ROOT_DIR}/Classes/Orm`))();
 
@@ -8,22 +9,13 @@ const orm = new (require(`${ROOT_DIR}/Classes/Orm`))();
 
     await orm.init();
     console.log('orm.init 1');
+
     await orm.initModels();
     console.log('orm.initModels 1');
+
     await router.init({orm});
-    // const newProduct = new orm.products(        {
-    //     isOnline: false,
-    //     title: 'Shine diamond',
-    //     count: {
-    //         buy: 10,
-    //         sell: 2
-    //     },
-    //     price: {
-    //         min: 200,
-    //         sell: 210,
-    //         current: 198
-    //     }
-    // });
-    // await newProduct.save();
-    console.log('finish');
+    console.log('router.init 1');
+
+    await parser.init({orm});
+    console.log('parser.init 1');
 })();
