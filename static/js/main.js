9666a2ab52451c8292af219883746d8cb23261d5
ITkah <ilatkacenko132@gmail.com>
2019-11-23 11:31:15 +0200

    доделал динамику

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,69 @@
 const parser = new Vue({
     el: '#parser',
     data: {
-        nameItem: '',
-        countItemMin: null,
-        countItemMax: null,
-        priceItemMin: null,
-        priceItemMax: null,
-        linkItem: '',
+        title: '',
+        count: {
+            buy: null,
+            sell: null
+        },
+        price: {
+            min: null,
+            sell: null,
+            curent: null
+        },
+        link: '',
         output: null,
+        isEdit: false,
+        button: "Добавить",
+        isLoading: true
     },
-    created() {
-        axios.get('/api/tasks ')
-            .then(response => (this.output = response.data))
-            .catch(error => (console.log(error)));
+    async created() {
+        this.output = (await new Promise((cb) => axios.get('/api/tasks').then(cb).then(this.isLoading = false).catch(error => console.log(error)))).data;
     },
     mounted() {
-        // setTimeout(function() {
-        //     if (this.status == false) {
-        //         this.error = true;
-        //     } else {
-        //         this.done = true;
-        //     }
-        // }, 0);
+
     },
     methods: {
-        sendItem() {
-            axios.post('/api/task', {
-                    body: [
-                        this.nameItem,
-                        this.countItemMin,
-                        this.countItemMax,
-                        this.priceItemMin,
-                        this.priceItemMax,
-                        this.linkItem
-                    ]
+        actionButton() {
+            this.isEdit ? this.methods.sendTask("put") : this.methods.sendTask("post");
+        },
+        sendTask(method) {
+            axios[method]('/api/task', {
+                    body: {
+                        title: this.nameItem,
+                        count: {
+                            buy: this.countItemMin,
+                            sell: this.countItemMax
+                        },
+                        price: {
+                            sell: this.priceItemMax,
+                            min: this.priceItemMin,
+                            current: null
+                        },
+                        link: this.linkItem
+                    }
                 })
                 .then(function(response) {
                     console.log(response);
+                    this.button = "Добавить";
+                    alert("Успешно отправленно");
                 })
                 .catch(function(error) {
                     console.log(error);
                 });
+        },
+        edit(id) {
+            this.isEdit = true;
+            this.output.forEach((el) => {
+                if (id === el._id) {
+                    this.title = el.title;
+                    this.count.buy = el.count.buy;
+                    this.count.sell = el.count.sell;
+                    this.price.min = el.price.min;
+                    this.price.max = el.price.sell;
+                    this.button = "Редактировать";
+                }
+            });
         }
     }
 });
